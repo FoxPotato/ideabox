c1e8526f19fec48bfe9055699a8e259039eacdab
Ivar Dijker <Skievar@gmail.com>
2017-12-18 15:18:36 +0100

    Finishing up design

--- main/static/main.js
+++ main/static/main.js
@@ -1,168 +1,15 @@
-// Variables for referencing the canvas and 2dcanvas context
-var canvas, ctx;
-
-// Variables to keep track of the mouse position and left-button status
-var mouseX, mouseY, mouseDown = 0;
-
-// Variables to keep track of the touch position
-var touchX, touchY;
-
-// Keep track of the old/last position when drawing a line
-// We set it to -1 at the start to indicate that we don't have a good value for it yet
-var lastX, lastY = -1;
-
-// Draws a line between the specified position on the supplied canvas name
-// Parameters are: A canvas context, the x position, the y position, the size of the dot
-function drawLine(ctx, x, y, size) {
-
-    // If lastX is not set, set lastX and lastY to the current position
-    if (lastX == -1) {
-        lastX = x;
-        lastY = y;
-    }
-
-    // Let's use black by setting RGB values to 0, and 255 alpha (completely opaque)
-    r = 0;
-    g = 0;
-    b = 0;
-    a = 255;
-
-    // Select a fill style
-    ctx.strokeStyle = "rgba(" + r + "," + g + "," + b + "," + (a / 255) + ")";
-
-    // Set the line "cap" style to round, so lines at different angles can join into each other
-    ctx.lineCap = "round";
-    //ctx.lineJoin = "round";
-
-
-    // Draw a filled line
-    ctx.beginPath();
-
-    // First, move to the old (previous) position
-    ctx.moveTo(lastX, lastY);
-
-    // Now draw a line to the current touch/pointer position
-    ctx.lineTo(x, y);
-
-    // Set the line thickness and draw the line
-    ctx.lineWidth = size;
-    ctx.stroke();
-
-    ctx.closePath();
-
-    // Update the last position to reference the current position
-    lastX = x;
-    lastY = y;
-}
-
-// Clear the canvas context using the canvas width and height
-function clearCanvas(canvas, ctx) {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-}
-
-// Keep track of the mouse button being pressed and draw a dot at current location
-function sketchpad_mouseDown() {
-    mouseDown = 1;
-    drawLine(ctx, mouseX, mouseY, 12);
-}
-
-// Keep track of the mouse button being released
-function sketchpad_mouseUp() {
-    mouseDown = 0;
-
-    // Reset lastX and lastY to -1 to indicate that they are now invalid, since we have lifted the "pen"
-    lastX = -1;
-    lastY = -1;
-}
-
-// Keep track of the mouse position and draw a dot if mouse button is currently pressed
-function sketchpad_mouseMove(e) {
-    // Update the mouse co-ordinates when moved
-    getMousePos(e);
-
-    // Draw a dot if the mouse button is currently being pressed
-    if (mouseDown == 1) {
-        drawLine(ctx, mouseX, mouseY, 12);
-    }
-}
-
-// Get the current mouse position relative to the top-left of the canvas
-function getMousePos(e) {
-    if (!e)
-        var e = event;
-
-    if (e.offsetX) {
-        mouseX = e.offsetX;
-        mouseY = e.offsetY;
-    }
-    else if (e.layerX) {
-        mouseX = e.layerX;
-        mouseY = e.layerY;
-    }
-}
-
-// Draw something when a touch start is detected
-function sketchpad_touchStart() {
-    // Update the touch co-ordinates
-    getTouchPos();
-
-    drawLine(ctx, touchX, touchY, 12);
-
-    // Prevents an additional mousedown event being triggered
-    event.preventDefault();
-}
-
-function sketchpad_touchEnd() {
-    // Reset lastX and lastY to -1 to indicate that they are now invalid, since we have lifted the "pen"
-    lastX = -1;
-    lastY = -1;
-}
-
-// Draw something and prevent the default scrolling when touch movement is detected
-function sketchpad_touchMove(e) {
-    // Update the touch co-ordinates
-    getTouchPos(e);
-
-    // During a touchmove event, unlike a mousemove event, we don't need to check if the touch is engaged, since there will always be contact with the screen by definition.
-    drawLine(ctx, touchX, touchY, 12);
-
-    // Prevent a scrolling action as a result of this touchmove triggering.
-    event.preventDefault();
-}
-
-// Get the touch position relative to the top-left of the canvas
-// When we get the raw values of pageX and pageY below, they take into account the scrolling on the page
-// but not the position relative to our target div. We'll adjust them using "target.offsetLeft" and
-// "target.offsetTop" to get the correct values in relation to the top left of the canvas.
-function getTouchPos(e) {
-    if (!e)
-        var e = event;
-
-    if (e.touches) {
-        if (e.touches.length == 1) { // Only deal with one finger
-            var touch = e.touches[0]; // Get the information for finger #1
-            touchX = touch.pageX - touch.target.offsetLeft;
-            touchY = touch.pageY - touch.target.offsetTop;
-        }
-    }
-}
-
-
-// Set-up the canvas and add our event handlers after the page has loaded
-function init() {
-    // Get the specific canvas element from the HTML document
-
-}
-
 Vue.component('id-canvas', {
         data: function () {
             return {lorem: "canvas", canvas: this.$refs.canvas}
         },
         template: `
-            <v-card color="green">
-            <v-card-title primary class="title" primary-title>Teken hier</v-card-title>
-                    <v-card-text v-text="lorem">
-                    </v-card-text>
+            <v-card class="full-height ma-2">
+                <v-toolbar dark color="teal">
+                        <v-toolbar-title>Teken hier</v-toolbar-title>
+                </v-toolbar>
+               
+                <v-card-text v-text="lorem">
+                </v-card-text>
             </v-card>
         `
     }
@@ -170,14 +17,16 @@ Vue.component('id-canvas', {
 
 Vue.component('id-info', {
         data: function () {
-            return {item: "idee"}
+            return {item: "idee", dialog: false, validForm: false, formInput: {student_number: "", sector: "", title: "", message: ""}}
         },
         template: `
-            <v-card color="green">
-                <v-card-title primary class="title">Campus ideeen</v-card-title>
+            <v-card class="full-height ma-2">
+                <v-toolbar dark color="teal">
+                    <v-toolbar-title>Campus ideeen</v-toolbar-title>
+                </v-toolbar>
                 <v-container>
                     <v-expansion-panel popout>
-                         <v-expansion-panel-content v-for="(item,i) in 5" :key="i">
+                         <v-expansion-panel-content v-for="(item,i) in 10" :key="i">
                             <div slot="header">hello</div>
                             <v-card>
                               <v-card-text class="grey lighten-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</v-card-text>
@@ -185,62 +34,62 @@ Vue.component('id-info', {
                          </v-expansion-panel-content> 
                     </v-expansion-panel>
                 </v-container>
-                <v-card-title primary class="title">Deel jouw idee!</v-card-title>
-                <v-container>
-                    <v-layout row wrap>
-                      <v-flex xs12 sm4>
-                        <p>Overflow</p>
-                        <v-select v-bind:items="dropdown_font" overflow label="Select"></v-select>
-                      </v-flex>
-                      <v-flex xs12 sm4>
-                        <p>Segmented</p>
-                        <v-select v-bind:items="dropdown_icon" label="Select" segmented></v-select>
-                      </v-flex>
-                      <v-flex xs12 sm4>
-                        <p>Editable</p>
-                        <v-select v-bind:items="dropdown_edit" label="Select" editable item-value="text"></v-select>
-                      </v-flex>
-                    </v-layout>
-                </v-container>
+                <v-dialog v-model="dialog" fullscreen transition="dialog-bottom-transition" :overlay="false">
+                    <v-card>
+                        <v-toolbar dark color="teal">
+                            <v-btn icon @click.native="dialog = false" dark>
+                                <v-icon>close</v-icon>
+                            </v-btn>
+                            <v-toolbar-title>Deel jouw idee!</v-toolbar-title>
+                            <v-spacer></v-spacer>                               
+                        </v-toolbar>
+                        <v-card-text>
+                            <v-form v-model="validForm" lazy-validation>
+                                <v-container grid-list-md>
+                                    <v-layout wrap>
+                                        <v-layout row wrap>
+                                            <v-flex xs6>
+                                                <v-text-field v-model="formInput.student_number" label="student number" required :rules="[v=>!!v || 'this field is required']"></v-text-field>
+                                            </v-flex>
+                                            <v-flex xs6>
+                                                <v-select v-model="formInput.sector" label="sector" autocomplete required :items="['Techniek', 'Economie', 'Zorg']" :rules="[v=>!!v || 'this field is required']"></v-select>
+                                            </v-flex>
+                                        </v-layout>
+                                        <v-flex xs12>
+                                            <v-text-field v-model="formInput.title" label="title" required :rules="[v=>!!v || 'this field is required']"></v-text-field>
+                                        </v-flex>
+                                        <v-flex xs12>
+                                            <v-text-field v-model="formInput.message" name="input-7-1" label="message" multi-line required :rules="[v=>!!v || 'this field is required']"></v-text-field>
+                                        </v-flex>
+                                    </v-layout>
+                                    <v-btn @click="submit" :disabled="!validForm">Submit</v-btn>
+                                </v-container>
+                            </v-form>
+                        </v-card-text>
+                    </v-card>
+                </v-dialog>
+                <v-btn block color="purple" dark @click.native="dialog = true">Deel jouw idee!</v-btn>  
             </v-card>`
 
     }
 );
 
 Vue.component('id-form', {
-        data: function () {
-            return {lorem: "form"}
-        },
-        template: `
-        
-            <v-card color="green">
-                <v-card-title primary class="title" primary-title>Zebra gesloten?</v-card-title>
-                <v-container>
-                    <v-expansion-panel popout>
-                         <v-expansion-panel-content v-for="(item,i) in 1" :key="i">
-                            <div slot="header">Maak een afspraak!</div>
-                            <v-card>
-                            <v-container>
-                                <v-layout row wrap>
-                                    <v-flex xs12 sm4>
-                                        <p>Overflow</p>
-                                        <v-select v-bind:items="dropdown_font" overflow label="Select"></v-select>
-                                    </v-flex>
-                                    <v-flex xs12 sm4>
-                                        <p>Segmented</p>
-                                        <v-select v-bind:items="dropdown_icon" label="Select" segmented></v-select>
-                                    </v-flex>
-                                    <v-flex xs12 sm4>
-                                        <p>Editable</p>
-                                        <v-select v-bind:items="dropdown_edit" label="Select" editable item-value="text"></v-select>
-                                    </v-flex>
-                                </v-layout>
-                            </v-container>
-                            </v-card>
-                         </v-expansion-panel-content> 
-                    </v-expansion-panel>
-                </v-container>
-            </v-card>`
+    data: function () {
+        return {lorem: "form"}
+    },
+    template: `
+        <v-card class="full-height ma-2 mt-3">
+            <v-toolbar dark color="teal">
+                <v-toolbar-title>Zebra geloten?</v-toolbar-title>
+            </v-toolbar>
+            <v-container>
+            <v-card>
+            
+            </v-card>
+            
+            </v-container>
+        </v-card>`
     }
 );
 
