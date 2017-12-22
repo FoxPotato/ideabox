49944aa6ce05e801d6cceaac4c71bdd79f1501e9
Ivar Dijker <Skievar@gmail.com>
2017-12-22 12:15:58 +0100

    Started with mdc

--- main/static/main.js
+++ main/static/main.js
@@ -1,223 +1,17 @@
-const Tokens = {
-    // Sequences that no-one will ever put in their keyboards.
-    PIPE: '7440011c983cf39ae730b1f831e2922ac5a76910',
-    OPEN_BRACE: 'f630c4abcae620278f82e142a526ef325c2a773a',
-    CLOSE_BRACE: 'ad982c66898e02a5dab419ea7568421b03f68ee2'
-};
-
-Vue.component('id-keyboard', {
-
-        props: {
-            value: {
-                type: String,
-                default: ''
-            },
-            layouts: {
-                type: [String, Array],
-                required: true
-            },
-            maxlength: {
-                type: Number,
-                default: 0,
-                validator: value => value >= 0
-            },
-            pattern: {
-                type: String,
-                default: null
-            }
-        },
-
-        data() {
-            return {
-                layout: 0
-            };
-        },
-
-        computed: {
-            /**
-             * Whether or not the keyboard input has hit its maximum length.
-             * @returns {Boolean}
-             */
-            full() {
-                return this.maxlength > 0 && this.value.length >= this.maxlength;
-            },
-
-            /**
-             * Whether or not the keyboard input is empty.
-             * @return {Boolean}
-             */
-            empty() {
-                return this.value.length === 0;
-            },
-
-            /**
-             * Returns the lines that make up a layout.
-             * @return {Array}
-             */
-            lines() {
-                let layout = (Array.isArray(this.layouts) ? this.layouts : [this.layouts])[this.layout];
-
-                return layout.replace('||', Tokens.PIPE)
-                    .replace('{{', Tokens.OPEN_BRACE)
-                    .replace('}}', Tokens.CLOSE_BRACE)
-                    .split('|');
-            },
-
-            /**
-             * Returns an array of buttons to render in the component.
-             * @returns {Array[]}
-             */
-            buttons() {
-                return this.lines.map(line => {
-                    // TODO: Could potentially rely on Object.values() here instead of being explicit.
-                    let stream = line.match(new RegExp(`(${Tokens.OPEN_BRACE}|${Tokens.CLOSE_BRACE}|${Tokens.PIPE}|.)`, 'g'));
-
-                    let buttons = [];
-                    let token = null;
-
-                    stream.forEach(char => {
-                        if (char === '{') {
-                            token = '';
-                        } else if (char === '}') {
-                            let command = token.split(':');
-                            let text = command.length > 1 ? command[0] : '';
-                            let action = command.length > 1 ? command[1] : command[0];
-                            let args = command.length > 2 ? command[2] : null;
-                            let method = null;
-
-                            if (['append', 'backspace', 'space', 'clear', 'goto'].indexOf(action) >= 0) method = this[action].bind(this, args);
-                            else method = this.$emit.bind(this, action, this);
-
-                            buttons.push({
-                                type: 'action',
-                                action: {name: action.replace(/\s+/g, '-').toLowerCase(), callable: method},
-                                value: text,
-                                args
-                            });
-
-                            token = null;
-                        } else {
-                            if (token !== null) {
-                                token += char;
-                            } else {
-                                if (char === Tokens.PIPE) char = '|';
-                                if (char === Tokens.OPEN_BRACE) char = '{';
-                                if (char === Tokens.CLOSE_BRACE) char = '}';
-
-                                buttons.push({
-                                    type: 'char',
-                                    action: {name: null, callable: this.append.bind(this, char)},
-                                    value: char,
-                                    args: null
-                                });
-                            }
-                        }
-                    });
-
-                    return buttons;
-                });
-            },
-
-            /**
-             * Whether or not the current value matches the regex provided to pattern. Always
-             * returns true if no pattern was provided.
-             * @returns {Boolean}
-             */
-            valid() {
-                return !this.pattern || this.value.match(new RegExp(this.pattern));
-            }
-        },
-
-        methods: {
-            /**
-             * Mutates the keyboard value to a new value.
-             * @param {String} value The new value.
-             */
-            mutate(value) {
-                if (this.maxlength > 0) {
-                    value = value.slice(0, this.maxlength);
-                }
-
-                this.$emit('input', value);
-            },
-
-            /**
-             * Appends a new value to the end of the current keyboard value.
-             * @param {String} char The character(s) to append.
-             */
-            append(char) {
-                this.mutate(this.value + char);
-            },
-
-            /**
-             * Remove the last character from the current keyboard value.
-             */
-            backspace() {
-                this.mutate(this.value.slice(0, this.value.length - 1));
-            },
-
-            /**
-             * Add one whitespace character to the current keyboard value.
-             */
-            space() {
-                this.append(' ');
-            },
-
-            /**
-             * Go to a new layout.
-             * @param {Number} The layout index.
-             */
-            goto(layout) {
-                if (Array.isArray(this.layouts)) {
-                    if (layout >= 0 && layout < this.layouts.length) {
-                        this.layout = layout;
-                    } else {
-                        throw new Error('The requested layout does not exist.');
-                    }
-                } else {
-                    throw new Error('A single non-array layout was provided.');
-                }
-            },
-
-            /**
-             * Clear the entire keyboard value.
-             */
-            clear() {
-                this.mutate('');
-            }
-        },
-        template: `
-        <aside class="vue-keyboard" role="application" :class="{ full: full, empty: empty, valid: valid, invalid: !valid }" :data-value="value" :data-layout="layout">
-            <div role="row" class="vue-keyboard-row" v-for="row in buttons" :data-keys="row.length">
-                <button
-                    v-for="btn in row"
-                    class="vue-keyboard-key"
-                    role="button"
-                    :class="btn.type"
-                    :data-args="btn.args"
-                    :data-text="btn.value"
-                    :data-action="btn.action.name"
-                    @click.prevent="btn.action.callable"
-                >{{ btn.value }}</button>
-            </div>
-        </aside>
-        `
-    }
-);
-
 Vue.component('id-canvas', {
         data: function () {
             return {lorem: "canvas", canvas: this.$refs.canvas}
         },
         template: `
-            <v-card class="ma-2">
-                <v-toolbar dark color="teal">
-                        <v-toolbar-title>Teken hier</v-toolbar-title>
-                </v-toolbar>
-               
-                <v-card-text v-text="lorem">
-                </v-card-text>
-            </v-card>
+            <div class="mdc-card">
+                <header class="mdc-toolbar">
+                    <div class="mdc-toolbar-row">
+                        <section class="mdc-toolbar__section">
+                            <span class="mdc-toolbar__title">Teken hier</span>
+                        </section>
+                    </div>
+                </header>
+            </div>
         `
     }
 );
@@ -261,55 +55,83 @@ Vue.component('id-ideas', {
             }
         },
         template: `
-            <v-card class="ma-2">
-                <v-toolbar dark color="teal">
-                    <v-toolbar-title>Campus ideeen</v-toolbar-title>
-                    <v-spacer></v-spacer>
-                    <v-btn color="purple" large dark @click.native="dialog = true">Deel jouw idee!</v-btn>
-                </v-toolbar>
-                <v-container>
-                    <v-expansion-panel popout>
-                         <v-expansion-panel-content v-for="(item, i) in items" :key="i">
-                            <div slot="header">{{item.title}}</div>
-                            <v-card>
-                              <v-card-text class="grey lighten-3">{{item.message}}</v-card-text>
-                            </v-card>
-                         </v-expansion-panel-content> 
-                    </v-expansion-panel>
-                </v-container>
-                <v-dialog v-model="dialog" fullscreen transition="dialog-bottom-transition" :overlay="false">
-                    <v-card>
-                        <v-toolbar dark color="teal">
-                            <v-btn icon @click.native="dialog = false" dark>
-                                <v-icon>close</v-icon>
-                            </v-btn>
-                            <v-toolbar-title>Deel jouw idee!</v-toolbar-title>
-                            <v-spacer></v-spacer>                               
-                        </v-toolbar>
-                        <v-card-text>
-                            <v-form v-model="validForm" lazy-validation>
-                                <v-container grid-list-md>
-                                    <v-layout row wrap>
-                                        <v-flex xs6>
-                                            <v-text-field @focus="focusInput($event, 'student_number')" type="text" data-layout="normal"  v-model="formInput.student_number" label="Stamnummer/gebruikersnaam" required :rules="[v=>!!v || 'this field is required']"></v-text-field>
-                                        </v-flex>
-                                        <v-flex xs6>
-                                            <v-select @focus="focusInput($event, 'sector')" type="text" data-layout="normal"  v-model="formInput.sector" label="Sector" autocomplete required :items="sectorItems" :rules="[v=>!!v || 'this field is required']"></v-select>
-                                        </v-flex>
-                                    </v-layout>
-                                    <v-flex xs12>
-                                        <v-text-field @focus="focusInput($event, 'title')" type="text" data-layout="normal"  v-model="formInput.title" label="Titel" required :rules="[v=>!!v || 'this field is required']"></v-text-field>
-                                    </v-flex>
-                                    <v-flex xs12>
-                                        <v-text-field @focus="focusInput($event, 'message')" type="text" data-layout="normal"  v-model="formInput.message" name="input-7-1" label="Bericht" multi-line required :rules="[v=>!!v || 'this field is required']"></v-text-field>
-                                    </v-flex>
-                                    <v-btn @click="submit" :disabled="!validForm">Submit</v-btn>
-                                </v-container>
-                            </v-form>
-                        </v-card-text>
-                    </v-card>
-                </v-dialog>
-            </v-card>`,
+            <div class="mdc-card">
+               <header class="mdc-toolbar">
+                   <div class="mdc-toolbar__row">
+                       <section class="mdc-toolbar__section mdc-toolbar__section--align-start">
+                           <span class="mdc-toolbar__title">Title</span>
+                       </section>
+                       <section class="mdc-toolbar__section mdc-toolbar__section--align-end">
+                           <button class="mdc-button mdc-button--unelevated secondary-filled-button">
+                               Raised disabled button
+                           </button>
+                       </section>
+                   </div>
+               </header>
+               <mdc-list bordered two-line>
+                    <a v-for="(item, i) in items" v-if="i < 7" href="" :key="i" class="mdc-ripple-surface mdc-list-item">         
+                        <span class="mdc-list-item__text">
+                            {{item.title}}
+                            <span class="mdc-list-item__text__secondary">{{item.message}}</span>
+                        </span>
+                    </a>
+                </mdc-list>
+            </div>
+            <!--<div class="mdc-card">-->
+                <!--<header class="mdc-toolbar">-->
+                  <!--<div class="mdc-toolbar__row">-->
+                    <!--<section class="mdc-toolbar__section mdc-toolbar__section&#45;&#45;align-start">-->
+                      <!--Section aligns to start.-->
+                    <!--</section>-->
+                    <!--<section class="mdc-toolbar__section">-->
+                      <!--Section aligns to center.-->
+                    <!--</section>-->
+                    <!--<section class="mdc-toolbar__section mdc-toolbar__section&#45;&#45;align-end">-->
+                      <!--Section aligns to end.-->
+                    <!--</section>-->
+                  <!--</div>-->
+                <!--</header>-->
+                <!--<mdc-list bordered two-line>-->
+                    <!--<a v-for="(item, i) in items" v-if="i < 7" href="" :key="i" class="mdc-ripple-surface mdc-list-item">         -->
+                        <!--<span class="mdc-list-item__text">-->
+                            <!--{{item.title}}-->
+                            <!--<span class="mdc-list-item__text__secondary">{{item.message}}</span>-->
+                        <!--</span>-->
+                    <!--</a>-->
+                <!--</mdc-list>-->
+                <!--<mdc-dialog ref="dialog" :overlay="false">-->
+                    <!--<v-card>-->
+                        <!--<v-toolbar dark color="teal">-->
+                            <!--<v-btn icon @click.native="dialog = false" dark>-->
+                                <!--<v-icon>close</v-icon>-->
+                            <!--</v-btn>-->
+                            <!--<v-toolbar-title>Deel jouw idee!</v-toolbar-title>-->
+                            <!--<v-spacer></v-spacer>                               -->
+                        <!--</v-toolbar>-->
+                        <!--<v-card-text>-->
+                            <!--<v-form v-model="validForm" lazy-validation>-->
+                                <!--<v-container grid-list-md>-->
+                                    <!--<v-layout row wrap>-->
+                                        <!--<v-flex xs6>-->
+                                            <!--<v-text-field @focus="focusInput($event, 'student_number')" type="text" data-layout="normal"  v-model="formInput.student_number" label="Stamnummer/gebruikersnaam" required :rules="[v=>!!v || 'this field is required']"></v-text-field>-->
+                                        <!--</v-flex>-->
+                                        <!--<v-flex xs6>-->
+                                            <!--<v-select @focus="focusInput($event, 'sector')" type="text" data-layout="normal"  v-model="formInput.sector" label="Sector" autocomplete required :items="sectorItems" :rules="[v=>!!v || 'this field is required']"></v-select>-->
+                                        <!--</v-flex>-->
+                                    <!--</v-layout>-->
+                                    <!--<v-flex xs12>-->
+                                        <!--<v-text-field @focus="focusInput($event, 'title')" type="text" data-layout="normal"  v-model="formInput.title" label="Titel" required :rules="[v=>!!v || 'this field is required']"></v-text-field>-->
+                                    <!--</v-flex>-->
+                                    <!--<v-flex xs12>-->
+                                        <!--<v-text-field @focus="focusInput($event, 'message')" type="text" data-layout="normal"  v-model="formInput.message" name="input-7-1" label="Bericht" multi-line required :rules="[v=>!!v || 'this field is required']"></v-text-field>-->
+                                    <!--</v-flex>-->
+                                    <!--<v-btn @click="submit" :disabled="!validForm">Submit</v-btn>-->
+                                <!--</v-container>-->
+                            <!--</v-form>-->
+                        <!--</v-card-text>-->
+                    <!--</v-card>-->
+                <!--</mdc-dialog>-->
+            <!--</div>-->`,
         created() {
             this.$http.get('/api/ideas/').then((response) => {
                 this.items = response.body;
